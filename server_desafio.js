109b75bb06f7f9b2af40afdb5c2aa7a8212ed8ed
Evelyn Suzarte <evelynsuzarte@hotmail.com>
2026-03-09 23:09:49 -0300

    docs: criacao de documentacao swagger

--- server_desafio.js
+++ server_desafio.js
@@ -1,12 +1,89 @@
 const express = require("express")
 const sqlite3 = require("sqlite3").verbose()
 const jwt = require("jsonwebtoken")
-
+const swaggerUi = require("swagger-ui-express")
+const swaggerJsdoc = require("swagger-jsdoc")
 
 const PORT = 3000
 const app = express()
 app.use(express.json())
 const SECRET = "admin"
+
+// configurações do swagger --------------------------------------------------------
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API de ordens e itens - Desafio Jitterbit 2026",
+            version: "1.0.0",
+            description: "Criada por: Evelyn Suzarte Fernandes"
+        },
+        servers: [
+            {
+                url: "http://localhost:3000"
+            }
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT"
+                }
+            },
+            schemas: {
+
+                Item: {
+                    type: "object",
+                    properties: {
+                        orderId: { type: "integer", example: 1 },
+                        productId: { type: "integer", example: 101 },
+                        quantity: { type: "integer", example: 2 },
+                        price: { type: "number", example: 30 }
+                    }
+                },
+
+                Order: {
+                    type: "object",
+                    properties: {
+                        orderId: { type: "integer", example: 1 },
+                        value: { type: "number", example: 120.5 },
+                        creationDate: { type: "string", example: "2026-03-09T10:30:00" },
+                        items: {
+                            type: "array",
+                            items: { $ref: "#/components/schemas/Item" }
+                        }
+                    }
+                },
+
+                LoginRequest: {
+                    type: "object",
+                    properties: {
+                        username: { type: "string", example: "admin" },
+                        password: { type: "string", example: "admin" }
+                    }
+                },
+
+                LoginResponse: {
+                    type: "object",
+                    properties: {
+                        token: { type: "string" }
+                    }
+                }
+            }
+        },
+        security: [
+            {
+                bearerAuth: []
+            }
+        ]
+    },
+    apis: ["./server_desafio.js"]
+}
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions)
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
 
 // inicialização do banco de dados --------------------------------------
 const db = new sqlite3.Database("pedidos.db")
@@ -21,7 +98,7 @@ CREATE TABLE IF NOT EXISTS orders (
 
 db.run(`
 CREATE TABLE IF NOT EXISTS items (
-    orderId INTEGER,
+    orderId INTEGER PRIMARY KEY,
     productId INTEGER,
     quantity INTEGER,
     price DOUBLE,
@@ -31,6 +108,29 @@ CREATE TABLE IF NOT EXISTS items (
 
 
 // autenticação -------------------------------
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: User login
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/LoginRequest'
+ *     responses:
+ *       200:
+ *         description: Token gerado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/LoginResponse'
+ *       401:
+ *         description: Credencial inválida
+ */
 
 //endpoint para fazer login
 app.post("/login", (req,res)=>{
@@ -67,18 +167,39 @@ function authenticateToken(req,res,next){
     })
 }
 
-// edpoints ---------------------------------------------------------
+// endpoints ---------------------------------------------------------
 
-//endpoint para adicionar ordem
+/**
+ * @swagger
+ * /order:
+ *   post:
+ *     summary: endpoint para adicionar ordem
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Order'
+ *     responses:
+ *       200:
+ *         description: Ordem criada
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Order'
+ *       500:
+ *         description: Error ao criar ordem
+ */
 app.post("/order", authenticateToken, (req, res) => {
     const { orderId, value, creationDate, items } = req.body
     db.run(
-        "INSERT INTO orders (orderId, value, creationDate) VALUES (?, ?, ?)",
-        [orderId, value, creationDate],
-        function(err){
+        "INSERT INTO orders (orderId, value, creationDate) VALUES (?, ?, ?)",[orderId, value, creationDate],function(err){
             if(err){
                 return res.status(500).json({
-                    "error": "Error creating order",
+                    "error": "Error ao criar ordem",
                     "detail": err.message
                 })
             }
@@ -101,8 +222,29 @@ app.post("/order", authenticateToken, (req, res) => {
     )
 })
 
-//endpoint para buscar todas as ordens
+
+/**
+ * @swagger
+ * /order/list:
+ *   get:
+ *     summary: endpoint para buscar todas as ordens
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Lista de ordens
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Order'
+ *       500:
+ *         description: Erro ao buscar ordens
+ */
 app.get("/order/list", authenticateToken, (req, res) => {
+
     // query de join entre as tabelas de ordens e items, com junção entre tabelas onde tenta encontrar os itens das ordens                                                                                                                                                                                  
     const query = `
         SELECT 
@@ -124,7 +266,6 @@ app.get("/order/list", authenticateToken, (req, res) => {
                 detail: err.message
             })
         }
-
         const orders = {}
         rows.forEach(row => {
             if (!orders[row.orderId]) {
@@ -149,7 +290,33 @@ app.get("/order/list", authenticateToken, (req, res) => {
     })
 })
 
-//endpoint para buscar ordem por id
+
+/**
+ * @swagger
+ * /order/{orderId}:
+ *   get:
+ *     summary: endpoint para buscar ordem por id
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: orderId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Ordem encontrada
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Order'
+ *       404:
+ *         description: Ordem ${orderId} não encontrada
+ *       500:
+ *         description: Erro ao buscar items
+ */
 app.get("/order/:orderId", authenticateToken, (req, res) => {
     const orderId = req.params.orderId
     db.get( "SELECT * FROM orders WHERE orderId = ?",[orderId],(err, order) => {
@@ -186,7 +353,26 @@ app.get("/order/:orderId", authenticateToken, (req, res) => {
 
 
 
-//endpoint para atualizar apenas o valor da ordem
+/**
+ * @swagger
+ * /order/{orderId}:
+ *   patch:
+ *     summary: endpoint para atualizar apenas o valor da ordem
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: orderId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Ordem atualizada
+ *       500:
+ *         description: Erro ao atualizar a ordem ${orderId}
+ */
 app.patch("/order/:orderId", authenticateToken, (req, res) => {
     const { orderId } = req.params
     const { value, creationDate } = req.body
@@ -207,7 +393,27 @@ app.patch("/order/:orderId", authenticateToken, (req, res) => {
 })
 
 
-//endpoint para deletar ordem
+
+/**
+ * @swagger
+ * /order/{orderId}:
+ *   delete:
+ *     summary: endpoint para deletar ordem
+ *     tags: [Orders]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: orderId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Ordem deletada
+ *       500:
+ *         description: Erro ao deletar a ordem ${orderId}
+ */
 app.delete("/order/:orderId", authenticateToken, (req, res) => {
     const { orderId } = req.params
     db.run("DELETE FROM items WHERE orderId = ?", [orderId])
